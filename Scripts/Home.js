8b22fd8011bb17f770b0cb5235792f9e4d6f9395
N. Praseeth <82506625+Praseeth2001@users.noreply.github.com>
2024-06-11 11:56:39 +0530

    Update Home.js

--- Scripts/Home.js
+++ Scripts/Home.js
@@ -1,17 +1,45 @@
+document.querySelector('.carousel-control.prev').addEventListener('click', function () {
+    const carousel = document.querySelector('.carousel');
+    const scrollAmount = 200;
+    const maxScrollLeft = carousel.scrollWidth - carousel.clientWidth;
+    const currentScrollLeft = carousel.scrollLeft;
 
-        document.querySelector('.carousel-control.prev').addEventListener('click', function () {
-            document.querySelector('.carousel').scrollBy({
-                top: 0,
-                left: -200,
-                behavior: 'smooth'
-            });
+    if (currentScrollLeft === 0) {
+        // If at the beginning, scroll to the end
+        carousel.scrollTo({
+            top: 0,
+            left: maxScrollLeft,
+            behavior: 'smooth'
         });
+    } else {
+        // Scroll left by the defined amount
+        carousel.scrollBy({
+            top: 0,
+            left: -scrollAmount,
+            behavior: 'smooth'
+        });
+    }
+});
 
-        document.querySelector('.carousel-control.next').addEventListener('click', function () {
-            document.querySelector('.carousel').scrollBy({
-                top: 0,
-                left: 200,
-                behavior: 'smooth'
-            });
+document.querySelector('.carousel-control.next').addEventListener('click', function () {
+    const carousel = document.querySelector('.carousel');
+    const scrollAmount = 200;
+    const maxScrollLeft = carousel.scrollWidth - carousel.clientWidth;
+    const currentScrollLeft = carousel.scrollLeft;
+
+    if (currentScrollLeft >= maxScrollLeft) {
+        // If at the end, scroll to the beginning
+        carousel.scrollTo({
+            top: 0,
+            left: 0,
+            behavior: 'smooth'
         });
-    
+    } else {
+        // Scroll right by the defined amount
+        carousel.scrollBy({
+            top: 0,
+            left: scrollAmount,
+            behavior: 'smooth'
+        });
+    }
+});
